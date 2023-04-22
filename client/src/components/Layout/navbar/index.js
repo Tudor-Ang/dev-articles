2031ba1a458a32ddc452a69a1ed17f07e8f9058e
tudorAng <tudoranis@yahoo.com>
2023-04-22 19:10:44 +0300

    fix: navbar background color

--- client/src/components/Layout/navbar/index.js
+++ client/src/components/Layout/navbar/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Nav, Logo } from './styles'
-
 import { Contain, Button } from '../../Collection';
+
 
 const Navbar = () => {
   return (
-    <Contain borderBottom={'1px solid lightGrey'}>
+    <Contain background={'#fff'} borderBottom={'1px solid lightGrey'}>
       <Nav>
         {/*TODO: change placeholder logo */}
         <Logo>Logo</Logo>
         <Contain>
-          <Button borderRadius={'7px'} color={'grey'} background={'transparent'} fontWeight={'300'} hoverBackground={'#EBECFC'} padding={'8px 20px 8px 20px'} hoverColor={'#3B49DF'}>Log in</Button>
-          <Button border={'1px solid #3B49DF'} color={'#3B49DF'} borderRadius={'7px'} background={'transparent'} padding={'8px 20px 8px 20px'} margin={'0 0 0 10px'} hoverBackground={'#3B49DF'} hoverColor={"#fff"}>Create account</Button>
+          <Link to="/auth/login">
+            <Button borderRadius={'7px'} color={'grey'} background={'transparent'} fontWeight={'300'} hoverBackground={'#EBECFC'} padding={'8px 20px 8px 20px'} hoverColor={'#3B49DF'}>Log in</Button>
+          </Link>
+          <Link to="/auth/register">
+            <Button border={'1px solid #3B49DF'} color={'#3B49DF'} borderRadius={'7px'} background={'transparent'} padding={'8px 20px 8px 20px'} margin={'0 0 0 10px'} hoverBackground={'#3B49DF'} hoverColor={"#fff"}>Create account</Button>
+          </Link>
         </Contain>
       </Nav>
     </Contain>
