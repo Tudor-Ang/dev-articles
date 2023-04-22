7e1bfa69f4732f59da53d4373b3e85b256e55fd9
tudorAng <tudoranis@yahoo.com>
2023-04-22 21:25:18 +0300

    feat: add P component

--- client/src/components/Collection.js
+++ client/src/components/Collection.js
@@ -58,6 +58,7 @@ export const Button = styled.button((props) => ({
   cursor: 'pointer',
   transition: 'all 0.3s ease-out',
   fontSize: props.fontSize ? props.theme?.toEm(props.fontSize) : props.theme?.toEm(14),
+  ...(props.width && { width: props.width }),
   ...(props.border && { border: props.border }),
   ...(props.borderRadius && { borderRadius: props.borderRadius }),
   ...(props.padding && { padding: props.padding }),
@@ -70,4 +71,14 @@ export const Button = styled.button((props) => ({
     ...(props.hoverColor && { color: props.hoverColor }),
     ...(props.hoverBorder && { border: props.hoverBorder }),
   },
+}));
+
+export const P = styled.p((props) => ({
+  color: props.theme.colors.main,
+  fontSize: props.fontSize ? props.theme?.toEm(props.fontSize) : props.theme?.toEm(14),
+  ...(props.weight && { fontWeight: props.weight }),
+  ...(props.align && { textAlign: props.align }),
+  ...(props.opacity && { opacity: props.opacity }),
+  ...(props.margin && { margin: props.margin }),
+  ...(props.padding && { padding: props.padding }),
 }));
