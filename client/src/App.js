6706e0618016c1e157f3d7bc09adbda092d71187
tudorAng <tudoranis@yahoo.com>
2023-04-22 05:19:01 +0300

    feat: add global theme

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,28 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import routes from './routes'
+import { ThemeProvider } from 'styled-components';
+import Theme from './theme'
 import './assets/styles/index.scss'
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        {routes.map(({ Layout, ...props }) => <Route path={props.path} element={<Layout {...props} />} />)}
-        {/* TODO: move 404 page to the routes.js file --> */}
-        <Route
-          path='*'
-          element={
-            <div>
-              <h2>404 Page not found etc</h2>
-            </div>
-          }
-        />
-      </Routes>
-    </Router>
+    <ThemeProvider theme={Theme}>
+      <Router>
+        <Routes>
+          {routes.map(({ Layout, ...props }) => <Route path={props.path} element={<Layout {...props} />} />)}
+          {/* TODO: move 404 page to the routes.js file --> */}
+          <Route
+            path='*'
+            element={
+              <div>
+                <h2>404 Page not found etc</h2>
+              </div>
+            }
+          />
+        </Routes>
+      </Router>
+    </ThemeProvider>
+
   );
 }
 
---
+++ client/src/theme.js
@@ -0,0 +1,9 @@
+const Theme = {
+  toEm: (px) => `${px / 14}em`,
+  colors: {
+    white: '#fffff',
+    main: '#10242C',
+  },
+};
+
+export default Theme;
