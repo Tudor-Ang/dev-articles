66445bb711e092f8f6d1e6f8cb5b7285a6755536
tudorAng <tudoranis@yahoo.com>
2023-04-23 20:11:20 +0300

    update sdk

---
+++ client/src/sdk/user.sdk.js
@@ -0,0 +1,15 @@
+/**
+* This is an auto generated code. This code should not be modified since the file can be overwriten
+* if new genezio commands are executed.
+*/
+
+import { Remote } from "./remote.js"
+
+export class User {
+  static remote = new Remote("http://127.0.0.1:8083/User")
+
+  static async create(username, email, password) {
+    return User.remote.call("User.create", username, email, password)
+  }
+
+}
