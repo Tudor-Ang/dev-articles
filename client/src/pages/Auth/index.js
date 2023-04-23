9621cceaf153535f348669b6e95ccfa02a5fcfc4
tudorAng <tudoranis@yahoo.com>
2023-04-23 23:53:03 +0300

    feat: add register api req

--- client/src/pages/Auth/index.js
+++ client/src/pages/Auth/index.js
@@ -1,25 +1,49 @@
 import React from 'react'
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { Formik, Form } from 'formik';
 import { Wrapper, Contain, P, Button } from '../../components/Collection'
 import Input from '../../components/Input'
 
+import { User } from '../../sdk/user.sdk'
+
 const Auth = () => {
   const { type } = useParams()
+  const navigate = useNavigate();
+
+  const handleSubmit = async ({ username, email, password }) => {
+    if (type === 'register') {
+      const res = await User.create(
+        username,
+        email,
+        password,
+      ).catch(err => {
+        console.log(err.error);
+        return;
+      });
+
+      if (!res.success) {
+        console.log(res.msg);
+        return;
+      } else {
+        navigate('/auth/login');
+      }
+    } else {
+      console.log('login')
+    }
+  }
+
 
   return (
     <Wrapper>
-      <Contain padding={'40px 48px 40px 48px'} borderRadius={'5px'} background={'#fff'} direction={'column'} maxWidth={'640px'} width={"100%"} margin={'20px auto'} border={'1px solid lightGrey'}>
+      <Contain padding={'40px 48px 40px 48px'} borderRadius={'5px'} background={'#fff'} direction={'column'} maxWidth={'640px'} width={'100%'} margin={'20px auto'} border={'1px solid lightGrey'}>
         <P fontSize={30} weight={700} align={'center'}>Welcome to Dev Articles</P>
         {/* TODO: add real data counter  */}
         <P opacity={'0.7'} fontSize={16} align={'center'}>Dev Articles is a community of 4 amazing developers </P>
 
         <Formik
           initialValues={{ email: '', password: '', username: '' }}
-          // validate={values => { }}
-          onSubmit={(values) => {
-            console.log(values)
-          }}
+          //TODO: add validations validate={values => { }}
+          onSubmit={(values) => handleSubmit(values)}
         >
           {({ handleChange, values }) => (
             <Form>
@@ -29,15 +53,13 @@ const Auth = () => {
                 {type === 'register' && <Input margin={'20px 0 0'} onChange={handleChange} value={values.username} name={'username'} label={'Username'} />}
                 <Input margin={'20px 0 0'} onChange={handleChange} value={values.password} name={'password'} label={'Password'} />
 
-                <Button type='submit' width={'100%'} padding={'10px'} color={'#fff'} background={'#3B49DF'} borderRadius={'5px'} height={'48px'} margin={'24px 0 0'}>Continue</Button>
+                <Button type='submit' width={'100%'} padding={'10px'} color={'#fff'} background={'#3B49DF'} borderRadius={'5px'} height={'48px'} margin={'24px 0 0'}>{type === 'register' ? 'Register' : 'Log In'}</Button>
               </Contain>
             </Form>
           )}
         </Formik>
 
-
       </Contain>
-
     </Wrapper>
   )
 }
