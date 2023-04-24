23e5843a59694503a94d6c1a91b990e0d6993ac6
tudorAng <tudoranis@yahoo.com>
2023-04-24 20:44:55 +0300

    feat: article

---
+++ server/models/article.js
@@ -0,0 +1,20 @@
+import { Schema, model } from 'mongoose';
+
+const articleSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  creator: {
+    type: String,
+    required: true,
+  },
+});
+
+const Article = model('Article', articleSchema);
+
+export default Article;
