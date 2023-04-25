aba6b316178d1a17a0cfa3e226b5587a54f84a29
tudorAng <tudoranis@yahoo.com>
2023-04-25 04:49:47 +0300

    feat: add data to post component

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,22 +1,27 @@
 import React from 'react'
-import { Contain, P } from './Collection'
+import { Contain, P, Image } from './Collection'
 import { Link } from 'react-router-dom';
+import moment from 'moment'
 
-const Post = () => {
+const Post = ({ data }) => {
+  const { creator, title, createdAt } = data || []
   return (
     <Link to={'/article/dkwakdka'}>
       <Contain hoverAnimation cursor={'pointer'} margin={'20px 0 0'}>
         <Contain direction={'column'} justify={'center'} minHeight={'227px'} width={'100%'} border={'1px solid lightgrey'} borderRadius={'5px'} background={'#fff'} padding={'20px'}>
           <Contain margin={'0 0 20px'} height={'50px'}>
-            <Contain border={'1px solid red'} width={'50px'} height={'50px'} borderRadius={'200px'} margin={'0 10px 0 0'}></Contain>
+            <Image width={'50px'} margin={'0 10px 0 0'} src={creator?.avatar} />
             <Contain direction={'column'} justify={'center'}>
-              <Link to={'/profile/jonSnow'}><P weight={500}>Jon Snow</P></Link>
-              <P weight={500}>Apr 22</P>
+              <Link to={`/profile/${creator?.username}`}><P hoverAnimation weight={500}>{creator?.username}</P></Link>
+              <P weight={500}>
+                {moment(createdAt).format('MMM D')}
+              </P>
             </Contain>
           </Contain>
 
           <Contain direction={'column'}>
-            <P fontSize={24} weight={700} margin={'0 0 20px'}>10 Popular Programming Languages to Learn in 2023: Python, JavaScript, Java, C#, Kotlin... </P>
+            <P fontSize={24} weight={700} margin={'0 0 20px'}>{title}</P>
+            {/* TODO: add selector for tags on article post */}
             <Contain flexWrap={'wrap'}>
               <P margin={'0 10px 0 0'} opacity={'0.6'} fontSize={12}>#webdev</P>
               <P margin={'0 10px 0 0'} opacity={'0.6'} fontSize={12}>#beginners</P>
